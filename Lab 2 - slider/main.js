8a963d717163b08250fd2b2bb0ac5b8b6fe92005
Subsaw <50599540+Subsaw@users.noreply.github.com>
2024-03-16 09:22:46 +0100

    Lab 2 - v 0.5

---
+++ Lab 2 - slider/main.js
@@ -0,0 +1,60 @@
+let slides = document.querySelector(".box").children;
+let nextSlide = document.querySelector(".right-slide");
+let prevSlide = document.querySelector(".left-slide");
+let totalSlides = slides.length;
+let index = 0;
+console.log(slides);
+
+nextSlide.onclick = function () {
+  next("next");
+};
+prevSlide.onclick = function () {
+  next("prev");
+};
+
+function next(direction) {
+  if (direction == "next") {
+    index++;
+    if (index == totalSlides) {
+      index = 0;
+    }
+  } else {
+    if (index == 0) {
+      index = totalSlides - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+}
+
+const show = (number) => {
+  index = number;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+};
+
+// const box = document.querySelector('.box')
+
+// setTimeout(() => {
+//   box.style.transform = 'translateX(200px)'
+// }, 2_000)
+// let position = 0
+// const anim = () => {
+//   box.style.transform = `translateX(${position}px)`
+//   position++
+//   setTimeout(anim, 16)
+// }
+// const animInterval = setInterval(anim, 16)
+
+// anim()
+
+// setTimeout(() => {
+//   clearInterval(animInterval)
+// }, 2_000)
